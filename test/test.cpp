00f4200829b7f23e75334d1d188020137f0be8bb
“alandiazv” <adiaz205@ucr.edu>
2024-05-03 09:59:10 -0700

    triangle tests completed and fixed

--- test/test.cpp
+++ test/test.cpp
@@ -22,3 +22,22 @@ TEST(TriangleTests, testKind1) {
     Triangle *aTriangle = new Triangle(15,15,15);
     EXPECT_EQ (aTriangle->getKind(), Triangle::Kind::EQUILATERAL);
 }
+
+TEST(TriangleTests, testDeath1) {
+    EXPECT_DEATH (Triangle *aTriangle = new Triangle(0.9,-0.1,0.7), "Does not satisfy triangle inequality");
+}
+
+TEST(TriangleTests, testKind2) {
+    Triangle *aTriangle = new Triangle(10,8,8);
+    EXPECT_EQ (aTriangle->getKind(), Triangle::Kind::ISOSCELES);
+}
+
+TEST(TriangleTests, testThrow1) {
+    Triangle *aTriangle = new Triangle(10,8,8);
+    EXPECT_NO_THROW (aTriangle->getKind());
+}
+
+TEST(TriangleTests, testKind3) {
+    Triangle *aTriangle = new Triangle(10,5,8);
+    EXPECT_EQ (aTriangle->getKind(), Triangle::Kind::SCALENE);
+}
