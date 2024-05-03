08a6df000637e90db5e40396b01db0419a0ac587
“alandiazv” <adiaz205@ucr.edu>
2024-05-02 17:23:02 -0700

    created first 3 failing triangle class tests

--- test/test.cpp
+++ test/test.cpp
@@ -5,5 +5,20 @@ using shapes::Triangle;
 
 TEST(TriangleTests, testPerimeter) {
     Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getPerimeter(),9);
+    EXPECT_EQ (aTriangle->getPerimeter(), 9);
+}
+
+TEST(TriangleTests, testPerimeter2) {
+    Triangle *aTriangle = new Triangle(10,10,5);
+    EXPECT_EQ (aTriangle->getPerimeter(), 25);
+}
+
+TEST(TriangleTests, testArea1) {
+    Triangle *aTriangle = new Triangle(15,12,6);
+    EXPECT_EQ (aTriangle->getArea(), 34.2);
+}
+
+TEST(TriangleTests, testKind1) {
+    Triangle *aTriangle = new Triangle(15,15,15);
+    EXPECT_EQ (aTriangle->getKind(), Triangle::Kind::EQUILATERAL);
 }
